b4c20980e11e71872296bdc5f57ca1a301f84062
JArcher <viewtiful.kja@gmail.com>
2022-06-21 18:58:36 -0400

    Adding start button

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,46 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score*/
+var questionBox = $('#questionBox');
+var quizQuestions = [
+    {
+        question: "",
+        answers: {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        },
+        rightAnswer: ''
+    }
+];
+
+
+function pushQuestions(questionSet) {
+    for (var i = 0; i < questionSet.length; i++) {
+        questionBox.append('<button class="btn btn-primary" type="button">Button</button>');
+    }
+}
+
+
+/*Score Button*/
+var scoreBoard = $('<button type="button" class="btn btn-outline-success">Score</button>');
+
+/*Score*/
+function genScore() {
+
+}
+
+//scoreBoard.text('test');
+//scoreBoard.attr('ahref', '')
+
+$('main').append(scoreBoard);
+
+function gameStart() {
+    $('section').append('<button id="startBtn">Start</button>');
+    $('#startBtn').on('click', pushQuestions(quizQuestions));
+
+}
+
+gameStart();
 
